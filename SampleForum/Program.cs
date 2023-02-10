d303fc753c24bc11d3f9261652142bbde3b1519d
Марат Сламгазизов <slamgazizov.m21@gmail.com>
2023-02-10 15:51:29 +0300

    comments added to program.cs

--- SampleForum/Program.cs
+++ SampleForum/Program.cs
@@ -8,16 +8,26 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    //при возникновении исключения/ошибки перенаправляет на "/error", чтобы сообщить 
+    //об этом пользователю
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //для безопасности, чтобы использовался ssl сертификат
+    //каждый раз когда сайт будет открываться через http, браузер будет перенаправлен 
+    //на https
     app.UseHsts();
 }
 
+//настройка параметров переадресации
 app.UseHttpsRedirection();
+
+//компонент для отправки статических файлов клиенту
 app.UseStaticFiles();
 
+//проверяет соответствие маршрута и
+//подбирает наиболее подходящие ответ на запрос по конкретному маршруту н-р: /profile
 app.UseRouting();
 
+//встроенный компонент для определения прав пользователя
 app.UseAuthorization();
 
 app.MapRazorPages();
